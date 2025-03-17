cb15584c7841f2831059be2ca11fa76098ea9a4c
MT1607 <nguyengiason5@gmail.com>
2025-03-17 22:57:01 +0700

    create right click in workspace of page storage

--- app/dashboard/storage/page.tsx
+++ app/dashboard/storage/page.tsx
@@ -1,6 +1,36 @@
+"use client"
+
+import {Item, Menu, useContextMenu} from "react-contexify";
+import "react-contexify/dist/ReactContexify.css";
+import {useRef} from "react";
+
+const MENU_ID = "context-menu"
 const StoragePage = () => {
+    const {show} = useContextMenu();
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const handleContextMenu = (event: React.MouseEvent) => {
+        if (containerRef.current && containerRef.current.contains(event.target as Node)) {
+            event.preventDefault();
+            show({event, id: MENU_ID});
+        }
+    };
     return (
-        <>Storage page</>
+        <div className="h-full m-4 overflow-auto rounded-lg p-3 bg-gray-100 shadow-md" onContextMenu={handleContextMenu}
+             ref={containerRef}
+        >
+            Storage page
+
+            {/* Context Menu */}
+            <Menu id={MENU_ID}>
+                <Item onClick={() => console.log("Tạo folder")}>📂 Tạo Folder</Item>
+                <Item onClick={() => console.log("Tải lên file")}>📤 Upload File</Item>
+                <Item onClick={() => console.log("Đổi tên folder")}>✏️ Đổi tên Folder</Item>
+                <Item onClick={() => console.log("Xóa folder")}>🗑️ Xóa Folder</Item>
+                <Item onClick={() => console.log("Mở folder")}>📂 Mở Folder</Item>
+                <Item onClick={() => console.log("Upload Folder")}>📂 Upload Folder</Item>
+            </Menu>
+        </div>
     )
 }
 
